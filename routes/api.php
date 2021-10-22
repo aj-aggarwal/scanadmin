d3e0efbae1f5c5fbe73e0ae845e3cfe84296fbb6
aj-aggarwal <ajay.agarwal173@gmail.com>
2021-10-22 14:09:05 +0530

    Built APIs to verify QR code and save user data

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ScanCodesController;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware(['client'])->group(function(){
+	Route::post('/scan_code', [ScanCodesController::class, 'scanCode']);
+	Route::post('/user', [UsersController::class, 'store']);
 });
